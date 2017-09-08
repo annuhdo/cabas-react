a19dcf04d68d7db9143e0530e285fdf145ca7075
Anna Ma <annahma@icloud.com>
2017-09-07 22:24:16 -0400

    added base_config file for re-base

---
+++ src/base_config.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: API_KEY_HERE,
+    authDomain: AUTH_DOMAIN_HERE,
+    databaseURL: DATABASE_URL_HERE,
+});
+
+export default base;
